2c0bf5ade7456a239d5b9e99cddee013d74600f2
Shar3d <shar3d@i.ua>
2018-06-14 14:47:03 +0300

    First commit

---
+++ lesson1/p1.py
@@ -0,0 +1,7 @@
+'''
+Created on Jun 14, 2018
+
+@author: shar3d
+'''
+name = input("Кто ты по жизни? ")
+print("Вечер в хату,", name)
---
+++ lesson2/p2.py
@@ -0,0 +1,8 @@
+num_1 = float (input("Enter first sum: "))
+num_2 = float (input("Enter second sum: "))
+res = float (num_1) + num_2
+Res = input("Enter something: ")
+Res *= 5
+print (Res)
+
+print("Result is", res)
